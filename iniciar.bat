b5b64355f0cb8b40fc1d9f353089d85cfbe3c2fb
Jose Fernandez <al02819593@tecmilenio.mx>
2022-02-19 03:07:17 -0600

    Crear registros

---
+++ iniciar.bat
@@ -0,0 +1,4 @@
+
+gradle clean build
+
+java -jar build/libs/evidencia-1.0-SNAPSHOT-all.jar
--- src/main/java/mx/tecmilenio/computacion/java/evidencia/util/Constantes.java
+++ src/main/java/mx/tecmilenio/computacion/java/evidencia/util/Constantes.java
@@ -5,7 +5,7 @@ import com.github.javafaker.Faker;
 public class Constantes {
     public static final int lineas = 80;
     public static final String PATRON_DIGITOS = "\\d+";
-    public static final String PATRON_ALFA_NUMERICO = "^[a-zA-Z0-9_\\.]+$";
+    public static final String PATRON_ALFA_NUMERICO = "^[a-zA-Z0-9_\\.\\-_\\s]+$";
 
     public static final Faker tester = new Faker();
 
